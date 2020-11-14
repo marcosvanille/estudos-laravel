f445025f147222ca640a662cf31170dbed626d25
marcosvanille <marcosvanillle@gmail.com>
2020-11-14 03:15:10 -0300

    [UPDATE]

--- Crud-laravel/resources/views/modelos/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <div class="col-md-12">
-            Voce esta visualizando o modelo : <strong> {{$modelo->modelo}}</strong>
-        </div>
-    </div>
-@endsection
--- laravel-projeto/resources/views/welcome.blade.php
+++ laravel-projeto/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Dev
                 </div>
 
                 <div class="links">
